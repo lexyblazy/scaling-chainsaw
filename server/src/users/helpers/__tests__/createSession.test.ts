a8d538b52cab9ad0d4cd9f46996e2fa195eae69e
lexyblazy <lexyblazy@gmail.com>
2021-07-08 08:15:50 +0100

    add test for createSession

---
+++ server/src/users/helpers/__tests__/createSession.test.ts
@@ -0,0 +1,33 @@
+import * as typeorm from 'typeorm';
+import { createSession } from '../createSession';
+import * as schemas from '../../../schemas';
+import { UserEntity } from '../../../schemas/types';
+import * as utils from '../../../utils';
+
+describe('users.helpers.createSession:', () => {
+  let typeormConnection: typeorm.Connection;
+  let usersRepository: typeorm.Repository<UserEntity>;
+  let user: UserEntity;
+
+  beforeAll(async () => {
+    typeormConnection = await utils.tests.helpers.createTypeormConnection();
+    await typeormConnection.synchronize(true);
+
+    usersRepository = typeormConnection.getRepository(schemas.user);
+  });
+
+  beforeEach(async () => {
+    await typeormConnection.synchronize(true);
+    user = await usersRepository.save(utils.tests.fakes.user);
+  });
+
+  afterAll(async () => {
+    await typeormConnection.close();
+  });
+
+  it('should return an object containing only token field', async () => {
+    const session = await createSession(user);
+    expect(session).toBeTruthy();
+    expect(session?.token).toBeDefined();
+  });
+});
