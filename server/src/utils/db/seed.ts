291ce4c4ff98e31f614f8634ced47bea19184fae
lexyblazy <lexyblazy@gmail.com>
2021-07-11 14:31:08 +0100

    improve seed function

--- server/src/utils/db/seed.ts
+++ server/src/utils/db/seed.ts
@@ -7,64 +7,71 @@
  * 3. Discount values are generated randomly with Math.random()
  *
  */
-
+import _ from 'lodash';
 import * as typeorm from 'typeorm';
 import * as schemas from '../../schemas';
 import { loadServicesAndConfig } from '../../initialize';
 
-const PROMO_CODES = [
-  'HALLOWEEN',
-  'FIT_BOY_SUMMER',
-  'CHOOSEBETTER',
-  'GLUTTON',
-  'A-TRUE-STEAL',
-  'MAKE-IT-RAIN',
-  'YOUTUBE-20',
-  'FATHERS-DAY-007'
-];
-
-const SERVICES: { name: string; description: string }[] = [
-  { name: 'KFC', description: "Finger-Lickin' Good" },
-  { name: 'WALMART', description: 'Save Money. Live Better.' },
+const SERVICES: { name: string; description: string; promoCodes: string[] }[] = [
+  {
+    name: 'KFC',
+    description: "Finger-Lickin' Good",
+    promoCodes: ['HALLOWEEN', 'FANTASTIC-FOUR', 'HI-SPEEDY', 'MAKE-IT-RAIN', 'BEN-10', 'UNCLES-007']
+  },
+  {
+    name: 'WALMART',
+    description: 'Save Money. Live Better.',
+    promoCodes: ['SUMMER-JOE', 'BLACK-N-BLUE', 'ONCE-IN-A-LIFETIME', 'MERKEL-GMBH-20', 'YOUTUBE-20', 'GI-JOE-007']
+  },
   {
     name: 'AMAZON',
-    description: 'Low prices you can trust'
+    description: 'Low prices you can trust',
+    promoCodes: ['FLYING-GOD', 'NARUTO-25', 'MIRACLES-40', 'MERKEL-GMBH-10', 'YOUTUBE-20', 'MS-BUILD']
   },
   {
     name: 'APPLE',
-    description: 'Simple, yet efficient'
+    description: 'Simple, yet efficient',
+    promoCodes: ['SAIKA-FESTIVAL', 'NEW_MOON-15', 'A-TRUE-STEAL', 'PLAYSTION-PLUS', 'SPOTIFY-PLUS', 'GOLDEN-DAWN']
   },
   {
     name: 'BESTBUY',
-    description: "You'll never get it cheaper elsewhere"
+    description: "You'll never get it cheaper elsewhere",
+    promoCodes: ['INDEPENDCE-20', 'LABOUR-DAY', 'QUEENS-VICTORIAL', 'WORLD-CUP-2019', 'APEX-20', 'FATHERS-DAY-007']
   },
   {
     name: "PAPA JOHN's",
-    description: 'The best pizza in the world'
+    description: 'The best pizza in the world',
+    promoCodes: ['SUPERNOVA', 'FIT_BOY_SUMMER', 'CHRISTMAS-20', 'I-CANT-STOP', 'FORNITE-LEGENDS', 'JAMES-BOND-O2']
   },
   {
     name: 'NIKE',
-    description: 'Just do it'
+    description: 'Just do it',
+    promoCodes: ['BOQUET', 'THEE-STALLION', 'HAPPY-WEDNESDAY', 'MAKE-IT-STOP', 'BATTLE-ROYALE', 'RECKON-MI6']
   },
   {
     name: 'PUMA',
-    description: "I'm supposed to be NIKE"
+    description: "I'm supposed to be NIKE",
+    promoCodes: ['KING-SIZE', 'YOUTUBE-10', 'VALENTINE-SPECIAL']
   },
   {
     name: 'ADIDAS',
-    description: "Three stripes don't make you Naruto"
+    description: "Three stripes don't make you Naruto",
+    promoCodes: ['ONE-PUNCH-MAN', 'ZEUS-30', 'INCURSIO', 'AKAME-GA-KILL']
   },
   {
     name: "DOMINO's PIZZA",
-    description: "Bloated pizza, it's cheap though"
+    description: "Bloated pizza, it's cheap though",
+    promoCodes: ['CHOOSEBETTER', 'GLUTTON', 'A-TRUE-STEAL', 'UFC-289']
   },
   {
     name: 'GUCCI',
-    description: 'Expensive garbage, but hey luxury!'
+    description: 'Expensive garbage, but hey luxury!',
+    promoCodes: ['HALLOWEEN', 'FIT_BOY_SUMMER']
   },
   {
     name: 'PRADA',
-    description: 'Just look good, nothing more!'
+    description: 'Just look good, nothing more!',
+    promoCodes: ['YOUTUBE-20', 'FATHERS-DAY-007']
   }
 ];
 
@@ -91,21 +98,22 @@ export const seed = async (existingConnection?: typeorm.Connection) => {
 
     console.log('Cleaned existing records');
 
-    const services = await servicesRepository.save(SERVICES);
+    const services = await servicesRepository.save(SERVICES.map(({ name, description }) => ({ name, description })));
 
     console.log('Services saved');
 
+    const servicesMap = _.keyBy(SERVICES, 'name');
     const promoCodeEntities: Partial<schemas.PromoCodeEntity>[] = [];
 
-    for (const code of PROMO_CODES) {
-      for (const service of services) {
+    for (const service of services) {
+      const promoCode = servicesMap[service.name].promoCodes;
+      for (const code of promoCode)
         promoCodeEntities.push({
           code,
           discountValue: Math.random().toFixed(2),
           status: 'active',
           service
         });
-      }
     }
 
     await promoCodesRepository.save(promoCodeEntities);
